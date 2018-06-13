60c1ff202ec1785fab17c42f2e8a6886ffc19e0d
Hitesh paliwal <pjhitesh007@gmail.com>
2018-06-13 14:33:34 +0530

    adding front end with react of lottery contract

---
+++ src/lottery.js
@@ -0,0 +1,8 @@
+import web3 from './web3';
+
+const address = '0x333ba9aC4f8336aDf5665372d8bA2bd4f133929c';
+
+const abi = [{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]
+
+export default new web3.eth.Contract(abi,address); //creating a local instance of deployed contract which will use in react app
+                                                   
---
+++ src/web3.js
@@ -0,0 +1,5 @@
+import Web3 from 'web3';
+
+const web3 = new Web3(window.web3.currentProvider); //this line give preconfigured web3 instance with v1.0
+
+export default web3;
